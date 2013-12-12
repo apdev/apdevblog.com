ef2e580658c68ca519a9a97caad21704d397e161
Aron Woost <aron@springworks.de>
2013-12-12 18:10:18 +0100

    Add gzipping to deployment process

---
+++ gzip_if_not_gzipped.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+file $1 | grep "gzip compressed data" > /dev/null
+# if [[ $? != 0 ]] ; then
+    # only gzip if it's not already gzipped
+    gzip -nf $1
+# fi
---
+++ gziped_sync.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# compress the files if they aren't
+find public/ -iname '*.html' -exec ./gzip_if_not_gzipped.sh {} \;
+find public/ -iname '*.js' -exec ./gzip_if_not_gzipped.sh {} \;
+find public/ -iname '*.css' -exec ./gzip_if_not_gzipped.sh {} \;
+
+# change their name back
+find public -iname '*.gz' -exec bash -c 'mv $0 ${0/.gz/}' {} \;
+echo "gzipping successful"
+
+echo "syncing gzipped files"
+s3cmd sync --progress --acl-public --reduced-redundancy --add-header 'Content-Encoding:gzip' public/* s3://$1/ --exclude '*.*' --include '*.html' --include '*.js' --include '*.css'
+echo "syncing gzipped files complete"
+
+echo "syncing non-gzipped files"
+s3cmd sync --progress --acl-public --reduced-redundancy --cf-invalidate public/* s3://$1/ --exclude '*.html' --exclude '*.js' --exclude '*.css'
+echo "syncing non-gzipped files complete"
