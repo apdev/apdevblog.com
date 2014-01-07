6e38837477fb71926fe16aa87a1b2739f0a42f86
Aron Woost <aron@springworks.de>
2014-01-07 10:35:08 +0100

    Reenable; don't zip already zipped assets

--- gzip_if_not_gzipped.sh
+++ gzip_if_not_gzipped.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 file $1 | grep "gzip compressed data" > /dev/null
-# if [[ $? != 0 ]] ; then
+if [[ $? != 0 ]] ; then
     # only gzip if it's not already gzipped
     gzip -nf $1
-# fi
+fi
