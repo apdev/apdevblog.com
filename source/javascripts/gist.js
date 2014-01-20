d2a648e339f55152b363d01d7a11198539d4a7b9
Philipp Kyeck <philipp@beta-interactive.de>
2014-01-20 10:55:49 +0100

    inline gist css

--- source/javascripts/gist.js
+++ source/javascripts/gist.js
@@ -13,11 +13,6 @@ function loadGists() {
     $.getJSON(name + '?callback=?', function(data) {
       var gist = gists[name];
       gist.data = data;
-      // Only insert the stylesheets once
-      if(stylesheets.indexOf(gist.data.stylesheet) < 0) {
-        stylesheets.push(gist.data.stylesheet);
-        $('head').append('<link rel="stylesheet" href="https://gist.github.com' + gist.data.stylesheet + '" />');
-      }
       gist.files = $(gist.data.div).find('.gist-file');
       gist.outer = $(gist.data.div).first().html('');
       // Iterate elements refering to this gist
