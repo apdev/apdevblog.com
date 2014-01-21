aec580a2e0a39fb2330a912b53371c675cea61d1
Aron Woost <aron@springworks.de>
2014-01-21 16:07:09 +0100

    Finalize vanilla gist.js

--- source/javascripts/gist.js
+++ source/javascripts/gist.js
@@ -1,50 +1,100 @@
-function loadGists() {
-  var els = $('a[data-json-url]'), gists = {}, code = [], stylesheets = [];
-  // Get elements referencing a gist and build a gists hash referencing the elements that use it
-  els.each(function(idx, rawEl) {
-    var el = $(rawEl), gist = el.attr('data-json-url');
-    rawEl.gist = gist;
-    rawEl.file = el.attr('data-file');
-    gists[gist] = gists[gist] || { targets: [] };
-    gists[gist].targets.push(el);
-  });
-  // Load the gists
-  $.each(gists, function(name, data) {
-    $.getJSON(name + '?callback=?', function(data) {
-      var gist = gists[name];
+var loadJSONP = (function(){
+  // Source: https://gist.github.com/132080/110d1b68d7328d7bfe7e36617f7df85679a08968
+  var unique = 0;
+  return function(url, callback, context) {
 
-      var gistMarkupFragment = document.createDocumentFragment();
-      // add div since you can't innerHTML on a documentFragment
-      var div = document.createElement("div");
-      div.innerHTML = data.div;
-      gistMarkupFragment.appendChild(div);
+    var originalUrl = url;
+
+    var name = "_jsonp_" + unique++;
+    if (url.match(/\?/)) url += "&callback="+name;
+    else url += "?callback="+name;
+
+    // Create script
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    // Setup handler
+    window[name] = function(data){
+      callback.call((context || window), data, originalUrl);
+      document.getElementsByTagName('head')[0].removeChild(script);
+      script = null;
+      delete window[name];
+    };
+    
+    // Load JSON
+    document.getElementsByTagName('head')[0].appendChild(script);
+  };
+})();
+
+function replaceElement(oldEl, newEl) {
+  var parent = oldEl.parentNode;
+  parent.innerHTML = '';
+  parent.appendChild(newEl);
+}
+
+function loadGists() {
+  var gists = {},
+    els = document.querySelectorAll('a[data-json-url]');
+
+  // Get elements referencing a gist and build a gists hash referencing 
+  // the elements that use it
+  [].forEach.call(els, function(el) {
+    var gist = {},
+      jsonUrl = el.getAttribute('data-json-url');
+
+    gist.el = el;
+    gist.file = el.getAttribute('data-file');
+
+    gists[jsonUrl] = gists[jsonUrl] || [];
+    gists[jsonUrl].push(gist);
+  });
+
+  // Load the gists
+  for(var key in gists) {
+    loadJSONP(key, function(data, url) {
+      var gist = gists[url],
+        gistMarkupFragment, helperDiv, gistFileEl, gistContainerElement;
+
+      gistMarkupFragment = document.createDocumentFragment();
+      // add div since you can't innerHTML on documentFragment
+      helperDiv = document.createElement('div');
+      helperDiv.innerHTML = data.div;
+      gistMarkupFragment.appendChild(helperDiv);
 
       // get all file div's
-      var gistFileEl = gistMarkupFragment.querySelectorAll(".gist-file");
+      gistFileEl = gistMarkupFragment.querySelectorAll('.gist-file');
 
       // get div that contains all file divs
-      var gistContainerElement = gistMarkupFragment.children.item(0).children.item(0);
-      gistContainerElement.innerHTML = "";
+      gistContainerElement = gistMarkupFragment.firstChild.firstChild;
 
       // Iterate elements refering to this gist
-      $(gist.targets).each(function(idx, target) {
-        var file = target.get(0).file;
+      gist.forEach(function(gistData) {
+        var file = gistData.file,
+          newGistElement;
+
         if(file) {
-          var o = gistContainerElement.cloneNode();
-          // TODO find matching file element
-          o.appendChild(gistFileEl.item(0));
+          newGistElement = gistContainerElement.cloneNode(false);
+          newGistElement.appendChild(gistFileEl.item(data.files.indexOf(file)));
 
           // override the anchor
-          var targetParent = target[0].parentNode;
-          targetParent.innerHTML = "";
-          targetParent.appendChild(o);
+          replaceElement(gistData.el, newGistElement);
         }
         else {
-          target.replaceWith(data.div);
+          newGistElement = gistContainerElement.cloneNode(true);
+          // override the anchor
+          replaceElement(gistData.el, newGistElement);
         }
       });
     });
-  });
+  }
 }
 
-loadGists();
+var feature = {
+  querySelectorAll: !!document.querySelectorAll,
+  array5: !!Array.prototype.forEach
+};
+
+if (feature.querySelectorAll && feature.array5) {
+  loadGists();
+}
