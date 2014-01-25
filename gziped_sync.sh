26c278109ffa7bb4eed5722ff3b59bd936848246
Aron Woost <aron@springworks.de>
2014-01-25 11:24:26 +0100

    Remove --progress from s3cmd, doesn't show very much of a progress

--- gziped_sync.sh
+++ gziped_sync.sh
@@ -10,9 +10,9 @@ find public -iname '*.gz' -exec bash -c 'mv $0 ${0/.gz/}' {} \;
 echo "gzipping successful"
 
 echo "syncing gzipped files"
-s3cmd sync --progress --acl-public --reduced-redundancy --cf-invalidate --verbose --cf-invalidate-default-index --add-header 'Content-Encoding:gzip' public/* s3://$1/ --exclude '*.*' --include '*.html' --include '*.js' --include '*.css'
+s3cmd sync --acl-public --reduced-redundancy --cf-invalidate --verbose --cf-invalidate-default-index --add-header 'Content-Encoding:gzip' public/* s3://$1/ --exclude '*.*' --include '*.html' --include '*.js' --include '*.css'
 echo "syncing gzipped files complete"
 
 echo "syncing non-gzipped files"
-s3cmd sync --progress --acl-public --reduced-redundancy --cf-invalidate --verbose public/* s3://$1/ --exclude '*.html' --exclude '*.js' --exclude '*.css'
+s3cmd sync --acl-public --reduced-redundancy --cf-invalidate --verbose public/* s3://$1/ --exclude '*.html' --exclude '*.js' --exclude '*.css'
 echo "syncing non-gzipped files complete"
