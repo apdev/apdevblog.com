227cf23e056fea806a820a24f2cd45c8910af575
Aron Woost <aron@springworks.de>
2014-01-27 16:13:04 +0100

    Fix cache-controll header and set default max-age to 12h

--- gziped_sync.sh
+++ gziped_sync.sh
@@ -10,17 +10,17 @@ find public -iname '*.gz' -exec bash -c 'mv $0 ${0/.gz/}' {} \;
 echo "gzipping successful"
 
 echo "syncing html (gzipped)"
-s3cmd sync --acl-public --reduced-redundancy --cf-invalidate --verbose --cf-invalidate-default-index --mime-type='text/html; charset=utf-8' --add-header 'Content-Encoding:gzip' --add-header 'Cache-Control:public;max-age=3600' public/* s3://$1/ --exclude '*.*' --include '*.html'
+s3cmd sync --acl-public --reduced-redundancy --cf-invalidate --verbose --cf-invalidate-default-index --mime-type='text/html; charset=utf-8' --add-header 'Content-Encoding:gzip' --add-header 'Cache-Control:public,max-age=43200' public/* s3://$1/ --exclude '*.*' --include '*.html'
 echo "syncing html (gzipped) complete"
 
 echo "syncing js and css (gzipped)"
-s3cmd sync --acl-public --reduced-redundancy --cf-invalidate --verbose --add-header 'Content-Encoding:gzip' --add-header 'Cache-Control:public;max-age=3600' public/* s3://$1/ --exclude '*.*' --include '*.js' --include '*.css'
+s3cmd sync --acl-public --reduced-redundancy --cf-invalidate --verbose --add-header 'Content-Encoding:gzip' --add-header 'Cache-Control:public,max-age=43200' public/* s3://$1/ --exclude '*.*' --include '*.js' --include '*.css'
 echo "syncing js and css (gzipped) complete"
 
 echo "syncing images"
-s3cmd sync --acl-public --reduced-redundancy --cf-invalidate --verbose --add-header 'Cache-Control:public;max-age=31536000' public/* s3://$1/ --exclude '*.*' --include '*.jpg' --include '*.png' --include '*.gif'
+s3cmd sync --acl-public --reduced-redundancy --cf-invalidate --verbose --add-header 'Cache-Control:public,max-age=31536000' public/* s3://$1/ --exclude '*.*' --include '*.jpg' --include '*.png' --include '*.gif'
 echo "syncing images complete"
 
 echo "syncing rest"
-s3cmd sync --acl-public --reduced-redundancy --cf-invalidate --verbose --add-header 'Cache-Control:public;max-age=3600' public/* s3://$1/ --exclude '*.html' --exclude '*.js' --exclude '*.css' --exclude '*.jpg' --exclude '*.png' --exclude '*.gif'
+s3cmd sync --acl-public --reduced-redundancy --cf-invalidate --verbose --add-header 'Cache-Control:public,max-age=43200' public/* s3://$1/ --exclude '*.html' --exclude '*.js' --exclude '*.css' --exclude '*.jpg' --exclude '*.png' --exclude '*.gif'
 echo "syncing rest complete"
